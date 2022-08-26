780f6b439d68509a0b47f02b113acb42cd5d4b3e
klisowska-wlog <86469730+klisowska-wlog@users.noreply.github.com>
2022-08-26 15:06:16 +0200

    Initial Machiavelli project structure Created 3 modules: Cards, Users and Game

---
+++ machiavelli-cards/src/main/java/pl/jellytech/machiavelli/cards/MachiavelliCardsApplication.java
@@ -0,0 +1,13 @@
+package pl.jellytech.machiavelli.cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachiavelliCardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MachiavelliCardsApplication.class, args);
+    }
+
+}
